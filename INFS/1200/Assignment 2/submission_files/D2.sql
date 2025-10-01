04ad9f383cb25f6168a768b985a8852c0c8c3d13
Caleb Yates <calebyates42@gmail.com>
2025-10-01 09:57:38 +0000

    add

--- INFS/1200/Assignment 2/submission_files/D2.sql
+++ INFS/1200/Assignment 2/submission_files/D2.sql
@@ -3,10 +3,26 @@ CREATE OR REPLACE VIEW NumFollowers AS
 		LEFT JOIN UserFollows ON UserBeingFollowed = User.Username
 		GROUP BY User.Username;
 
--- People Allan doesn't yet follow
-SELECT PotentialUser.Username FROM
-	(SELECT UserBeingFollowed as Username FROM UserFollows WHERE UserBeingFollowed != "Allan"
-		EXCEPT
-		SELECT UserBeingFollowed FROM UserFollows WHERE UserFollowing = "Allan") as PotentialUser
-	INNER JOIN NumFollowers ON NumFollowers.Username = PotentialUser.Username
-	ORDER BY NumFollowers.FollowerCount DESC LIMIT 5;
+SELECT * FROM NumFollowers;
+
+-- For each Username, how many people they follow that Allan also follows
+CREATE OR REPLACE VIEW MutualsWithAllan AS
+	SELECT User.Username, Count(UserFollows.UserFollowing) as MutualsCount FROM User
+		LEFT JOIN UserFollows ON UserFollows.UserFollowing = User.Username
+			AND UserFollows.UserBeingFollowed IN
+				-- All people following Allan
+				(SELECT UserFollowing FROM UserFollows WHERE UserBeingFollowed = "Allan")
+		GROUP BY User.Username;
+
+SELECT * FROM MutualsWithAllan;
+
+-- Rank every user except Allan himself
+SELECT * FROM User
+	LEFT JOIN MutualsWithAllan ON MutualsWithAllan.Username = User.Username
+	LEFT JOIN UserFollows ON UserFollows.UserBeingFollowed = "Allan" AND UserFollowing = User.Username
+	LEFT JOIN NumFollowers ON NumFollowers.Username = User.Username
+	WHERE User.Username != "Allan" -- Don't suggest Allan himself
+	ORDER BY MutualsWithAllan.MutualsCount DESC, -- Mutuals first
+	User.Username IN -- then people Allan already follows
+	(SELECT UserFollowing FROM UserFollows WHERE UserBeingFollowed = "Allan") DESC,
+	NumFollowers.FollowerCount DESC; -- tiebreaker who follows the most people
