45d664194c99f95136d67732c3537c3a97db4ef5
Caleb Yates <calebyates42@gmail.com>
2025-10-01 02:44:27 +0000

    feat: A9 done!

--- INFS/1200/Assignment 2/submission_files/A9-todo.sql
+++ INFS/1200/Assignment 2/submission_files/A9-todo.sql
@@ -1,10 +1,9 @@
 -- Counting reactions to any of Allan's posts by username
 CREATE OR REPLACE VIEW AllanReactions AS
 	SELECT User.Username, COUNT(PostReaction.Username) as ReactionsCount FROM User
-	--  LEFT so that if no matching post reactions exist, counts as 0
-		LEFT JOIN PostReaction ON PostReaction.PostId = Post.Id,
 		INNER JOIN Post ON Post.Username = "Allan"
-		WHERE PostReaction.Username = User.Username
+	--  LEFT so that if no matching post reactions exist, counts as 0
+		LEFT JOIN PostReaction ON PostReaction.PostId = Post.Id AND PostReaction.Username = User.Username
 		GROUP BY User.Username;
 
 SELECT * FROM AllanReactions;
@@ -12,14 +11,14 @@ SELECT * FROM AllanReactions;
 -- Counting comments to any of Allan's posts by username
 CREATE OR REPLACE VIEW AllanComments AS
 	SELECT User.Username, COUNT(Comment.Username) as CommentsCount FROM User
+		INNER JOIN Post ON Post.Username = "Allan"
 	--  LEFT so that if no matching comments exist, counts as 0
-		LEFT JOIN Comment ON Comment.Username = User.Username
-		INNER JOIN Post ON Post.Username = "Allan" AND Comment.PostId = Post.Id
+		LEFT JOIN Comment ON Comment.Username = User.Username AND Comment.PostId = Post.Id
 		GROUP BY User.Username;
 
 SELECT * FROM AllanComments;
 
-SELECT COUNT(UserFollows.UserFollowing) FROM UserFollows
+SELECT COUNT(UserFollows.UserFollowing) as NumStalkers FROM UserFollows
 	INNER JOIN AllanReactions ON AllanReactions.Username = UserFollows.UserFollowing
 	INNER JOIN AllanComments ON AllanComments.Username = UserFollows.UserFollowing
 	WHERE UserFollows.UserBeingFollowed = "Allan"
