2e262a7b434b445c6605ee5ee35e973eda0ae799
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2025-10-22 09:46:52 +1000

    add: shows graph

--- STAT/1301/Research Assignment/data.r
+++ STAT/1301/Research Assignment/data.r
@@ -1,1 +1,20 @@
 data = read.csv("Data.csv")
+data$Music = factor(data$Music, levels = c("Silent", "Classical"))
+data$Cups = factor(data$Cups, levels = c("None", "Medium", "Many"))
+
+library(lattice)
+
+# cex:
+# ‘cex’ A numeric multiplier to control character sizes for
+# axis labels.  Can be a vector of length 2
+
+# 4 = cross, 1 = circle
+# cross = silent, cirlce = classical
+chs = c(rep(4,18),rep(1,18)) # define two groups of plotting characters
+
+stripplot(Percentage~Cups, pch=chs, cex=1.5, data=data, xlab="Cups")
+
+6 chs = c(rep(4,9),rep(1,9)) # define two groups of plotting characters
+7 stripplot(Yield∼Fertilizer,pch=chs,cex=1.5,data=crop,xlab="Fertilizer")
+8 #stripplot(Yield∼Fertilizer,groups=Pesticide,cex=1.5,data=crop,
+9 # xlab="Fertilizer")
