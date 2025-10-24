0deaf593d8f50c5430310fb5a09c6c9aa87d36e8
Caleb Yates <calebyates42@gmail.com>
2025-10-24 08:11:04 +0000

    add

--- STAT/1301/Research Assignment/data.r
+++ STAT/1301/Research Assignment/data.r
@@ -4,15 +4,11 @@ data$Cups = factor(data$Cups, levels = c("None", "Medium", "Many"))
 
 library(lattice)
 
-# cex:
-# ‘cex’ A numeric multiplier to control character sizes for
-# axis labels.  Can be a vector of length 2
-
 # 4 = cross, 1 = circle, 3 = plus
 # cross = silent, cirlce = classical
 music_choices = c(rep(4,18),rep(1,18)) # define two groups of plotting characters
 # cross = none, plus = medium, circle = many
-cups_choices = c(rep(c(rep(4, 6), rep(3, 6), rep(1, 6)), 2))
+#cups_choices = c(rep(c(rep(4, 6), rep(3, 6), rep(1, 6)), 2))
 
 stripplot(Percentage~Cups, pch=music_choices, cex=1.5, data=data, xlab="Cups", main="Plot of percentage recall versus cups")
 # stripplot(Percentage~Music, pch=cups_choices, cex=1.5, data=data, xlab="Music")
@@ -35,20 +31,23 @@ pairwise.t.test(data$Percentage, data$Cups, p.adjust.method = "bonferroni")
 
 
 #** Testing most extreme cases **
-null = data[data$Music == "Silent" & data$Cups == "None",]
-extreme = data[data$Music == "Classical" & data$Cups == "Many",]
-t.test(null$Percentage, extreme$Percentage)
-pairwise.t.test(null$Percentage, extreme$Percentage, p.adjust.method = "bonferroni")
+# null = data[data$Music == "Silent" & data$Cups == "None",]
+# extreme = data[data$Music == "Classical" & data$Cups == "Many",]
+# t.test(null$Percentage, extreme$Percentage)
+# pairwise.t.test(null$Percentage, extreme$Percentage, p.adjust.method = "bonferroni")
 
 silent = data[data$Music == "Silent",]
-classical = data[data$Music == "Classical",]
-t.test(silent$Percentage, classical$Percentage) # not significant
+# classical = data[data$Music == "Classical",]
+# t.test(silent$Percentage, classical$Percentage) # not significant
 
-none = data[data$Cups == "None",]
-many = data[data$Cups == "Many",]
-t.test(none$Percentage, many$Percentage)
+# none = data[data$Cups == "None",]
+# many = data[data$Cups == "Many",]
+# t.test(none$Percentage, many$Percentage)
 
 # Only for silent
-anova(lm(Percentage~Cups, data=silent))
-# Only for classical
-anova(lm(Percentage~Cups, data=classical))
+data.lm_silent = lm(Percentage~Cups, data=silent)
+anova(data.lm_silent)
+plot(data.lm_silent)
+
+# # Only for classical
+# anova(lm(Percentage~Cups, data=classical))
